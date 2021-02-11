f3e7602ff229bfd697b42dd0d65bf5f459da3369
outland.karasu <outland.karasu@gmail.com>
2021-02-11 22:35:21 +0900

    issue #1 add empty.

--- source/karasux/nullable.d
+++ source/karasux/nullable.d
@@ -14,7 +14,7 @@ Params:
 Returns:
    content reference. 
 */
-ref inout(T) front(T)(scope return ref inout(Nullable!T) x) pure nothrow @safe
+ref inout(T) front(T)(auto scope return ref inout(Nullable!T) x) pure nothrow @safe
 in (!x.isNull)
 {
     return x.get;
@@ -25,6 +25,8 @@ in (!x.isNull)
 {
     import std.typecons : nullable;
 
+    assert(99.nullable.front == 99);
+
     auto x = 100.nullable;
     assert(x.front == 100);
     
@@ -34,5 +36,32 @@ in (!x.isNull)
     immutable v = 9876;
     auto nv = v.nullable;
     assert(nv.front == 9876);
+}
+
+/**
+return is nullable empty.
+
+Params:
+    T = content type.
+    x = nullable value.
+Returns:
+    true if nullable is empty.
+*/
+bool empty(T)(auto scope ref const(Nullable!T) x) pure nothrow @safe
+{
+    return x.isNull;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    import std.typecons : nullable;
+
+    auto x = 100.nullable;
+    assert(x.empty == false);
+    x.nullify;
+    assert(x.empty == true);
+
+    assert(typeof(x).init.empty == true);
 }
 
