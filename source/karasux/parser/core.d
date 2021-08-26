5abf56163ecd7aa25c4f22e80e205f715ff1342a
outland.karasu <outland.karasu@gmail.com>
2021-08-27 08:43:20 +0900

    issue #49 add any character parser.

--- source/karasux/parser/core.d
+++ source/karasux/parser/core.d
@@ -3,7 +3,10 @@ Core parsers module.s
 */
 module karasux.parser.core;
 
-import std.range : empty, isInputRange;
+import std.range :
+    empty,
+    popFront,
+    isInputRange;
 import std.traits : Unqual;
 
 /**
@@ -79,5 +82,36 @@ bool endOfSource(R)(auto scope ref R r)
     auto source = "aaa";
     assert(!source.endOfSource);
     assert(source == "aaa");
+}
+
+/**
+any character parser.
+
+Params:
+    r = input range.
+Returns:
+    true if range is not empty.
+*/
+bool any(R)(auto scope ref R r)
+    if (isInputRange!(Unqual!R))
+{
+    if (r.empty)
+    {
+        return false;
+    }
+
+    r.popFront();
+    return true;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    assert(!"".any);
+    assert("a".any);
+
+    auto source = "abc";
+    assert(source.any);
+    assert(source == "bc");
 }
 
