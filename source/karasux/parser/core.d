4e8e02bdde974c25ab4eb75773fca5e7e5be25b0
outland.karasu <outland.karasu@gmail.com>
2021-08-26 09:01:27 +0900

    issue #49 add always true paraser.

--- source/karasux/parser/core.d
+++ source/karasux/parser/core.d
@@ -3,4 +3,31 @@ Core parsers module.s
 */
 module karasux.parser.core;
 
+import std.range : isInputRange;
+import std.traits : Unqual;
+
+/**
+Always true parser.
+
+Params:
+    r = input range.
+Returns:
+    always true.
+*/
+bool parseAlwaysTrue(R)(auto scope ref R r)
+    if (isInputRange!(Unqual!R))
+{
+    return true;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    assert("".parseAlwaysTrue);
+    assert("aaa".parseAlwaysTrue);
+
+    auto source = "aaa";
+    assert(source.parseAlwaysTrue);
+    assert(source == "aaa");
+}
 
