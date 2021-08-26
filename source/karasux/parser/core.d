97357556f7731cc6c49025b5eda054e237c68c78
outland.karasu <outland.karasu@gmail.com>
2021-08-27 08:37:44 +0900

    issue #49 add always false.

--- source/karasux/parser/core.d
+++ source/karasux/parser/core.d
@@ -31,3 +31,28 @@ bool alwaysTrue(R)(auto scope ref R r)
     assert(source == "aaa");
 }
 
+/**
+Always false parser.
+
+Params:
+    r = input range.
+Returns:
+    always false.
+*/
+bool alwaysFalse(R)(auto scope ref R r)
+    if (isInputRange!(Unqual!R))
+{
+    return false;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    assert(!"".alwaysFalse);
+    assert(!"aaa".alwaysFalse);
+
+    auto source = "aaa";
+    assert(!source.alwaysFalse);
+    assert(source == "aaa");
+}
+
