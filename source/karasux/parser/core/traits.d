034e703a63d38415838a089fab5520ebdb285d2b
outland.karasu <outland.karasu@gmail.com>
2022-07-04 00:29:59 +0900

    issue #49 fix parser trait.

--- source/karasux/parser/core/traits.d
+++ source/karasux/parser/core/traits.d
@@ -40,5 +40,23 @@ enum isForwardRangeSource(R) = isInputSource!R && isForwardRange!(Unqual!R);
 /**
 Parser traits.
 */
-enum isParser(alias P, S) = is(typeof((scope ref S s) => P(s))) && !is(ReturnType!((S s) => P(s)) == bool);
+enum isParser(alias P, S) = is(typeof((scope ref S s) => P(s))) && is(ReturnType!((S s) => P(s)) == bool);
+
+///
+@nogc nothrow pure @safe unittest
+{
+    bool f(S)(scope auto ref S s)
+        if (isInputSource!S)
+    {
+        return true;
+    }
+
+    static assert(isParser!(f, string));
+    static assert(!isParser!(f, int));
+
+    void notParser(S)(scope auto ref S s)
+    {
+    }
+    static assert(!isParser!(notParser, string));
+}
 
