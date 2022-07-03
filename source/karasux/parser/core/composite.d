8b9b0caaef61aa4695b954e016ccf6857642a9d5
outland.karasu <outland.karasu@gmail.com>
2022-07-04 00:45:36 +0900

    issue #49 add semantic action.

--- source/karasux/parser/core/composite.d
+++ source/karasux/parser/core/composite.d
@@ -21,7 +21,14 @@ enum SemanticEventType
 /**
 Semantic action trait.
 */
-enum isSemanticAction(alias A, S) = isInputSource!S && is(typeof((auto scope ref const(S) s) => A(s, SemanticEventType.init)));
+enum isSemanticAction(alias A, S) = isInputSource!S && is(typeof((const(S) s) { A(s, SemanticEventType.init); }));
+
+///
+@nogc nothrow pure @safe unittest
+{
+    void f(string s, SemanticEventType event) { }
+    static assert(isSemanticAction!(f, string));
+}
 
 /**
 Parse with semantic action.
@@ -34,28 +41,42 @@ Params:
 Returns:
     parsing result
 */
-bool action(S, alias P, alias A)(auto scope ref S source)
+bool action(alias P, alias A, S)(auto scope ref S source)
     if (isParser!(P, S) && isSemanticAction!(A, S))
 {
-    return false;
+    A(source, SemanticEventType.begin);
+    if (P(source))
+    {
+        A(source, SemanticEventType.accept);
+        return true;
+    }
+    else
+    {
+        A(source, SemanticEventType.reject);
+        return false;
+    }
 }
 
-/+
 ///
 @nogc nothrow pure @safe unittest
 {
     import karasux.parser.core.primitive : any;
     import karasux.parser.core.traits : isParser;
 
-    static assert(isParser!(any, string));
     auto lastEvent = SemanticEventType.reject;
-    void f(SemanticEventType event)
+    void f(string s, SemanticEventType event)
     {
         lastEvent = event;
     }
 
     auto source = "test";
     assert(source.action!(any, f));
+    assert(source == "est");
     assert(lastEvent == SemanticEventType.accept);
+
+    source = "";
+    assert(!source.action!(any, f));
+    assert(source == "");
+    assert(lastEvent == SemanticEventType.reject);
 }
-+/
+
