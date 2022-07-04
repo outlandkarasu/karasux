64faa5d6f853be3a597c3e0b5e80d538e9adefde
outland.karasu <outland.karasu@gmail.com>
2022-07-05 08:28:30 +0900

    issue #49 split action module.

--- source/karasux/parser/package.d
+++ source/karasux/parser/package.d
@@ -3,6 +3,7 @@ Parser combinator package.
 */
 module parser;
 
+public import karasux.parser.action;
 public import karasux.parser.ast;
 public import karasux.parser.composite;
 public import karasux.parser.primitive;
