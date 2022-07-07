1d2a42547f98436207bf93cc4f631f38ab383223
outland.karasu <outland.karasu@gmail.com>
2022-07-08 00:36:15 +0900

    issue #49 implement node building.

--- source/karasux/parser/source/ast_builder_source.d
+++ source/karasux/parser/source/ast_builder_source.d
@@ -3,6 +3,8 @@ AST builder source module.
 */
 module karasux.parser.source.ast_builder_source;
 
+import core.memory : pureCalloc, pureRealloc, pureFree;
+
 import karasux.parser.source.traits :
     isInputSource,
     isSeekableSource;
@@ -55,6 +57,20 @@ struct ASTBuilderSource(R, T)
         this.inner = source;
     }
 
+    // not copyiable.
+    @disable this(this);
+
+    // not copyiable.
+    @disable this(ref return scope ASTBuilderSource rhs);
+
+    ~this() @nogc nothrow pure @safe scope
+    {
+        foreach (ref node; nodes_)
+        {
+            node.release();
+        }
+    }
+
     @nogc nothrow pure @safe scope
     {
         @property size_t nodePosition() const
@@ -69,9 +85,43 @@ struct ASTBuilderSource(R, T)
     Params:
         tag = AST node tag value
         start = node start position
+    Returns:
+        true if succeeded. false if failed allocation.
     */
-    void acceptNode(Tag tag, size_t start) @nogc nothrow pure @safe scope
+    bool acceptNode(Tag tag, size_t start) @nogc nothrow pure @trusted scope
+        in(start <= nodePosition_)
+        in(start <= nodes_.length)
     {
+        auto node = ASTNode!Tag(tag);
+
+        // append single node.
+        immutable oldLength = nodes_.length;
+        if (start == oldLength)
+        {
+            immutable newLength = oldLength + 1;
+            auto newNodes = cast(ASTNode!(Tag)*) pureRealloc(
+                (oldLength > 0) ? &nodes_[0] : null,
+                ASTNode!Tag.sizeof * newLength);
+            if (!newNodes)
+            {
+                return false;
+            }
+
+            nodes_ = newNodes[0 .. newLength];
+            nodes_[start] = node;
+            ++nodePosition_;
+            return true;
+        }
+
+        // move children from current nodes.
+        if (!node.moveChildren(nodes_[start .. nodePosition_]))
+        {
+            return false;
+        }
+
+        nodes_[start] = node;
+        nodePosition_ = start + 1;
+        return true;
     }
 
     R inner;
@@ -80,6 +130,7 @@ struct ASTBuilderSource(R, T)
 
 private:
     size_t nodePosition_;
+    ASTNode!(Tag)[] nodes_;
 }
 
 ///
@@ -88,7 +139,20 @@ private:
     import karasux.parser.source.array_source : arraySource;
 
     auto source = astBuilder!int(arraySource("test"));
+
     static assert(isASTBuilderSource!(typeof(source)));
+    assert(source.front == 't');
+    assert(source.position == 0);
+    assert(source.nodePosition == 0);
+    assert(!source.empty);
+
+    source.popFront();
+    assert(source.acceptNode(123, 0));
+
+    assert(source.front == 'e');
+    assert(source.position == 1);
+    assert(source.nodePosition == 1);
+    assert(!source.empty);
 }
 
 /**
@@ -102,5 +166,134 @@ Returns:
 ASTBuilderSource!(R, T) astBuilder(T, R)(return scope R r)
 {
     return ASTBuilderSource!(R, T)(r);
+}
+
+private:
+
+/**
+AST Node.
+*/
+struct ASTNode(Tag)
+{
+    /**
+    Construct node.
+
+    Params:
+        tag = node tag value
+    */
+    this(Tag tag) @nogc nothrow pure @safe scope
+    {
+        this.tag_ = tag;
+    }
+
+    @nogc nothrow pure @safe
+    {
+        @property Tag tag() const scope
+        {
+            return tag_;
+        }
+
+        @property const(ASTNode)[] children() const return scope
+        {
+            return children_;
+        }
+    }
+
+    /**
+    Allocate and copy new children.
+
+    Params:
+        fromChildren = new children source array
+    Returns:
+        true if succeeded. false if failed memory allocation.
+    */
+    bool moveChildren(return scope ASTNode[] fromChildren) @nogc nothrow pure @trusted scope
+    {
+        if (fromChildren.length == 0)
+        {
+            release();
+            return true;
+        }
+
+        immutable newLength = fromChildren.length;
+        auto newPointer = cast(ASTNode*) pureCalloc(newLength, ASTNode.sizeof);
+        if (!newPointer)
+        {
+            return false;
+        }
+
+        auto newArray = newPointer[0 .. newLength];
+        foreach (i, ref e; newArray)
+        {
+            e = fromChildren[i];
+
+            // remove old reference.
+            fromChildren[i].clearWithoutRelease();
+        }
+
+        release();
+        children_ = newArray;
+        return true;
+    }
+
+    /**
+    Release allocated memory.
+    */
+    void release() @nogc nothrow pure @trusted scope
+        out(; children_.length == 0)
+    {
+        foreach (ref e; children_)
+        {
+            e.release();
+        }
+
+        if (children_.length > 0)
+        {
+            pureFree(&children_[0]);
+        }
+
+        children_ = children_.init;
+    }
+
+private:
+
+    void clearWithoutRelease() @nogc nothrow pure @safe scope
+    {
+        tag_ = tag_.init;
+        children_ = children_.init;
+    }
+
+    Tag tag_;
+    ASTNode[] children_;
+}
+
+///
+nothrow pure @safe unittest
+{
+    scope node = ASTNode!int(123);
+    assert(node.tag == 123);
+    assert(node.children.length == 0);
+
+    auto children = [ASTNode!int(456)];
+    auto grandchild = [ASTNode!int(789)];
+    (() @nogc {
+        assert(children[0].moveChildren(grandchild));
+        assert(children[0].children.length == 1);
+        assert(children[0].children[0].tag == 789);
+    })();
+
+    (() @nogc {
+        assert(node.moveChildren(children));
+        assert(node.children.length == 1);
+        assert(node.children[0].tag == 456);
+        assert(node.children[0].children.length == 1);
+        assert(node.children[0].children[0].tag == 789);
+
+        // removed from children.
+        assert(children[0].children.length == 0);
+    })();
+
+    (() @nogc => node.release())();
+    assert(node.children.length == 0);
 }
 
