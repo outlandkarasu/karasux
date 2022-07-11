2da07a1300b83e7a0bc9d39529ef8337f957c7f0
outland.karasu <outland.karasu@gmail.com>
2022-07-12 00:05:22 +0900

    issue #49 implement AST node builder.

--- source/karasux/parser/source/ast_builder_source.d
+++ source/karasux/parser/source/ast_builder_source.d
@@ -3,7 +3,10 @@ AST builder source module.
 */
 module karasux.parser.source.ast_builder_source;
 
-import karasux.parser.source.traits : isInputSource;
+import karasux.buffer : Buffer;
+import karasux.parser.source.traits :
+    isInputSource,
+    isSeekableSource;
 
 /**
 AST builder source trait.
@@ -11,8 +14,18 @@ AST builder source trait.
 enum isASTBuilderSource(R) = isInputSource!R
   && is(typeof(R.Tag.init))
   && is(typeof((scope ref R r) @nogc nothrow pure @safe => r.nodePosition))
-  && is(typeof((scope ref R r) { auto p = r.nodePosition; r.acceptNode(R.Tag.init, p); }))
+  && is(typeof((scope ref R r) => r.startNode(R.Tag.init)))
+  && is(typeof((scope ref R r) => r.acceptNode(R.Tag.init)))
   && is(typeof((scope ref R r) { auto p = r.nodePosition; r.rejectNode(p); }));
+
+/**
+AST node event type.
+*/
+enum ASTNodeEventType
+{
+    start,
+    end
+}
 
 /**
 AST builder source.
@@ -29,6 +42,20 @@ struct ASTBuilderSource(R, T)
     AST tag type.
     */
     alias Tag = T;
+
+    static if (isSeekableSource!R)
+    {
+        alias PositionType = typeof(R.init.position);
+    }
+    else
+    {
+        alias PositionType = size_t;
+
+        @property size_t position() const @nogc nothrow pure @safe scope
+        {
+            return nodePosition;
+        }
+    }
 
     /**
     Initialize from inner source.
@@ -50,5 +77,69 @@ struct ASTBuilderSource(R, T)
     R inner;
 
     alias inner this;
+
+    @nogc nothrow pure @safe scope
+    {
+        @property size_t nodePosition() const
+        {
+            return events_.length;
+        }
+
+        bool startNode(Tag tag)
+        {
+            return events_.append(NodeEvent(tag, ASTNodeEventType.start, position));
+        }
+
+        bool acceptNode(Tag tag)
+        {
+            return events_.append(NodeEvent(tag, ASTNodeEventType.end, position));
+        }
+
+        bool rejectNode(size_t startNodePosition)
+        {
+            return events_.resize(startNodePosition);
+        }
+    }
+
+private:
+
+    struct NodeEvent
+    {
+        Tag tag;
+        ASTNodeEventType type;
+        PositionType position;
+    }
+
+    Buffer!NodeEvent events_;
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    import karasux.parser.source.array_source : arraySource;
+
+    auto source = astBuilder!int(arraySource("test"));
+    static assert(isASTBuilderSource!(typeof(source)));
+
+    assert(source.front == 't');
+    assert(source.position == 0);
+    assert(source.nodePosition == 0);
+    assert(!source.empty);
+}
+
+/**
+Create AST builder source.
+
+Params:
+    R = inner source type.
+    T = tag type.
+    source = inner source
+Returns:
+    AST builder source.
+*/
+ASTBuilderSource!(R, T) astBuilder(T, R)(return scope R source)
+    if (isInputSource!R)
+{
+    return ASTBuilderSource!(R, T)(source);
 }
 
