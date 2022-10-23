708856eb4f68055cccf813ff9c7e0e2a2ac09030
outland.karasu <outland.karasu@gmail.com>
2022-10-23 18:12:12 +0900

    issue #65 add memory type.

--- source/karasux/allocator.d
+++ source/karasux/allocator.d
@@ -4,11 +4,42 @@ Allocator types module.
 module karasux.allocator;
 
 /**
+Allocated memory.
+*/
+struct Memory(A)
+{
+    private void[] payload_;
+
+    void[] opCast(T: void[])() @nogc nothrow pure @safe
+    {
+        return payload_;
+    }
+
+    const(void)[] opCast(T: const(void)[])() const @nogc nothrow pure @safe
+    {
+        return payload_;
+    }
+}
+
+///
+@nogc nothrow pure @safe unittest
+{
+    struct A;
+
+    auto memory = Memory!A([]);
+    auto m = cast(void[]) memory;
+    static assert(is(typeof(m) == void[]));
+
+    auto cm = cast(const(void)[]) memory;
+    static assert(is(typeof(cm) == const(void)[]));
+}
+
+/**
 Allocator traits.
 */
 enum isAllocator(A) = is(typeof((ref A a) {
     auto allocator = a;
-    void[] data = allocator.allocate(cast(size_t) 100);
+    Memory!A data = allocator.allocate(cast(size_t) 100);
     allocator.free(data);
 }));
 
@@ -17,35 +48,35 @@ enum isAllocator(A) = is(typeof((ref A a) {
 {
     struct Allocator
     {
-        void[] allocate(size_t n) @nogc nothrow pure @safe scope
+        Memory!Allocator allocate(size_t n) @nogc nothrow pure @safe scope
         {
-            return (void[]).init;
+            return Memory!Allocator.init;
         }
 
-        void free(ref void[] memory) @nogc nothrow pure @safe scope
+        void free(ref Memory!Allocator memory) @nogc nothrow pure @safe scope
         {
-            memory = (void[]).init;
+            memory = Memory!Allocator.init;
         }
     }
 
     static assert(isAllocator!Allocator);
 
-    struct WithoutFree
+    struct MissingFree
     {
-        void[] allocate(size_t n) @nogc nothrow pure @safe scope
+        Memory!MissingFree allocate(size_t n) @nogc nothrow pure @safe scope
         {
-            return (void[]).init;
+            return Memory!MissingFree.init;
         }
     }
-    static assert(!isAllocator!(WithoutFree));
+    static assert(!isAllocator!(MissingFree));
 
-    struct WithoutAllocate
+    struct MissingAllocate
     {
-        void free(ref void[] memory) @nogc nothrow pure @safe scope
+        void free(ref Memory!MissingAllocate memory) @nogc nothrow pure @safe scope
         {
-            memory = (void[]).init;
+            memory = Memory!MissingAllocate.init;
         }
     }
-    static assert(!isAllocator!(WithoutAllocate));
+    static assert(!isAllocator!(MissingAllocate));
 }
 
