f5274c22eaf407ae57909cb5aee0965b2c3a8f95
outland.karasu <outland.karasu@gmail.com>
2022-10-23 17:17:09 +0900

    issue #65 add allocator type.

--- source/karasux/allocator.d
+++ source/karasux/allocator.d
@@ -3,3 +3,49 @@ Allocator types module.
 */
 module karasux.allocator;
 
+/**
+Allocator traits.
+*/
+enum isAllocator(A) = is(typeof((ref A a) {
+    auto allocator = a;
+    void[] data = allocator.allocate(cast(size_t) 100);
+    allocator.free(data);
+}));
+
+///
+@nogc nothrow pure @safe unittest
+{
+    struct Allocator
+    {
+        void[] allocate(size_t n) @nogc nothrow pure @safe scope
+        {
+            return (void[]).init;
+        }
+
+        void free(ref void[] memory) @nogc nothrow pure @safe scope
+        {
+            memory = (void[]).init;
+        }
+    }
+
+    static assert(isAllocator!Allocator);
+
+    struct WithoutFree
+    {
+        void[] allocate(size_t n) @nogc nothrow pure @safe scope
+        {
+            return (void[]).init;
+        }
+    }
+    static assert(!isAllocator!(WithoutFree));
+
+    struct WithoutAllocate
+    {
+        void free(ref void[] memory) @nogc nothrow pure @safe scope
+        {
+            memory = (void[]).init;
+        }
+    }
+    static assert(!isAllocator!(WithoutAllocate));
+}
+
