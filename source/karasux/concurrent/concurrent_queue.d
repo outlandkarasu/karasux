a593c46276ef4507afb81daf577243787894b1fc
outland.karasu <outland.karasu@gmail.com>
2022-10-27 22:43:12 +0900

    issue #67 add module.

---
+++ source/karasux/concurrent/concurrent_queue.d
@@ -0,0 +1,5 @@
+/**
+Concurrent queue module.
+*/
+module karasux.concurrent.concurrent_queue;
+
--- source/karasux/concurrent/package.d
+++ source/karasux/concurrent/package.d
@@ -3,3 +3,5 @@ Concurrent package.
 */
 module karasux.concurrent;
 
+public import karasux.concurrent.concurrent_queue;
+
